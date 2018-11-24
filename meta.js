2cf024e1a25e20d3cb5958d8cc3f0243106fd5b1
yangxin2 <yang657850144@qq.com>
2018-11-24 17:28:57 +0800

    fix: modify meta infos

---
+++ meta.js
@@ -0,0 +1,31 @@
+module.exports = {
+  prompts: {
+    name: {
+      name: 'projectName',
+      message: 'Project Name？',
+      default: ':projectName:'
+    },
+    description: {
+      name: 'projectDesc',
+      message: 'Project Description？',
+      default: ':projectName:'
+    },
+    version: {
+      name: 'version',
+      message: 'Project Version？',
+      default: ':version:'
+    },
+    username: {
+      name: 'yourname',
+      message: `Your Name？`,
+      default: ':gitUser:'
+    },
+    email: {
+      name: 'youremail',
+      message: `Your Email？`,
+      default: ':gitEmail:'
+    }
+  },
+  filters: {
+  }
+}

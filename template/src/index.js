501a10b6afd59afcb93e5c872b5ae965df711aab
yangxin2 <yang657850144@qq.com>
2018-11-25 15:24:49 +0800

    fix: modify basic vue template

--- template/src/index.js
+++ template/src/index.js
@@ -1,11 +1,11 @@
-const img = document.createElement('img')
-img.src = 'https://avatars2.githubusercontent.com/u/45163886?s=400&u=72e70470a7f4cc42bff9f054dcee0d8cfce57d58&v=4'
-img.style.display = 'block'
-img.style.margin = '0 auto'
-img.style.width = '200px'
-img.style.height = '200px'
-document.body.appendChild(img)
-const h1 = document.createElement('h1')
-h1.innerText = 'hello FETP'
-h1.style.textAlign = 'center'
-document.body.appendChild(h1)
+import Vue from 'vue'
+import App from '@/module/App'
+
+function init () {
+  new Vue({ // eslint-disable-line
+    el: '#app',
+    render: h => h(App)
+  })
+}
+
+init()

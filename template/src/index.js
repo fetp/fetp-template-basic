2143b4ed0888831f352d9fae5fcf2bb39431fccc
yangxin2 <yang657850144@qq.com>
2018-11-25 15:18:57 +0800

    fix: modify template

--- template/src/index.js
+++ template/src/index.js
@@ -1,3 +1,10 @@
+const img = document.createElement('img')
+img.src = 'https://avatars2.githubusercontent.com/u/45163886?s=400&u=72e70470a7f4cc42bff9f054dcee0d8cfce57d58&v=4'
+img.style.display = 'block'
+img.style.margin = '0 auto'
+img.style.width = '200px'
+img.style.height = '200px'
+document.body.appendChild(img)
 const h1 = document.createElement('h1')
 h1.innerText = 'hello FETP'
 h1.style.textAlign = 'center'

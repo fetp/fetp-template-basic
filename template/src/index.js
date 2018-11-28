b752205b9c582ce33198eee91a635a8cdebaa02f
yangxin2 <yang657850144@qq.com>
2018-11-28 22:26:22 +0800

    feat: use fetp in vue2.0

--- template/src/index.js
+++ template/src/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
-console.log('vUE', Vue)
-// import App from './module/App.vue' // eslint-disable-line
+import App from '@/module/App.vue' // eslint-disable-line
 
-// new Vue({ // eslint-disable-line
-//   el: '#app',
-//   render: h => h(App)
-// })
+new Vue({ // eslint-disable-line
+  el: '#app',
+  render: h => h(App)
+})
---
+++ template/src/module/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <HelloFetp :text="text"/>
+  </div>
+</template>
+
+<script>
+import HelloFetp from '@/component/HelloFetp'
+export default {
+  name: 'app',
+  data () {
+    return {
+      text: 'Hello FETP'
+    }
+  },
+  components: {
+    HelloFetp
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/css/index.scss';
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 350px;
+  line-height: 350px;
+  color: #fff;
+  font-size: 72px;
+  font-weight: bold;
+  text-align: center;
+  background: #000;
+}
+.logo {
+  width: 150px;
+  height: 150px;
+}
+</style>

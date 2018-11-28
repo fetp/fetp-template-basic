32018ffba5cc967427bec818acf899bf31b134e7
yangxin2 <yang657850144@qq.com>
2018-11-28 19:26:23 +0800

    test basic template

--- template/src/index.js
+++ template/src/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-import App from '@/module/App'
+console.log('vUE', Vue)
+// import App from './module/App.vue' // eslint-disable-line
 
-function init () {
-  new Vue({ // eslint-disable-line
-    el: '#app',
-    render: h => h(App)
-  })
-}
-
-init()
+// new Vue({ // eslint-disable-line
+//   el: '#app',
+//   render: h => h(App)
+// })
